7bccd91ab5086b7fa8b0163dbdd3ee6ca11f1173
GreattitJY <great.tit.jaejae@gmail.com>
2022-11-28 22:02:07 +0900

    Feat: ProductImg styled-components 작업

--- shopping-mall/src/components/Card/ProductImg/ProductImg.jsx
+++ shopping-mall/src/components/Card/ProductImg/ProductImg.jsx
@@ -1,6 +1,8 @@
+import { ProductImgStyle } from "./ProductImgStyle";
+
 export const ProductImg = ({productName, thumbnailImg}) => {
     const url = "http://test.api.weniv.co.kr/" + thumbnailImg;
     return (
-        <img src={url} alt={productName} />
+        <ProductImgStyle src={url} alt={productName} />
     )
 }
---
+++ shopping-mall/src/components/Card/ProductImg/ProductImgStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ProductImgStyle = styled.img`
+    /* position: relative; */
+    width: 380px;
+    height: 380px;
+    left: 0px;
+    top: 0px;
+    border: 1px solid #bdbdbd;
+    border-radius: 10px;
+`;
